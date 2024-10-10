6fe49067e6f993b7931bb75b721b53a429c1c69b
Ura <mikeneko41@icloudd.com>
2024-10-10 18:59:29 +0900

    rails6に変更

--- bookers/db/schema.rb
+++ bookers/db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_06_092401) do
+ActiveRecord::Schema.define(version: 2024_10_10_094820) do
+
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
--- bookers/test/controllers/books_controller_test.rb
+++ bookers/test/controllers/books_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
 class BooksControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get books_new_url
+  test "should get edit" do
+    get books_edit_url
     assert_response :success
   end
 
@@ -11,13 +11,13 @@ class BooksControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get show" do
-    get books_show_url
+  test "should get new" do
+    get books_new_url
     assert_response :success
   end
 
-  test "should get edit" do
-    get books_edit_url
+  test "should get show" do
+    get books_show_url
     assert_response :success
   end
 end
